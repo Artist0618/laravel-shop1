17ce8d0a777fcfdf3e35538c3d56aa899894f128
jzy <17875058835@163.com>
2020-06-29 10:11:43 +0800

    No remarks

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,10 +25,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //修改策略自动发现的逻辑
-        Gate::guessPolicyNamesUsing(function ($modelClass){
-            //动态返回模型对应的策略名称
-            return 'App\Policies\\'.class_basename($modelClass).'Policy';
+        // 使用 Gate::guessPolicyNamesUsing 方法来自定义策略文件的寻找逻辑
+        Gate::guessPolicyNamesUsing(function ($class) {
+            // class_basename 是 Laravel 提供的一个辅助函数，可以获取类的简短名称
+            // 例如传入 \App\Models\User 会返回 User
+            return '\\App\\Policies\\'.class_basename($class).'Policy';
         });
     }
 }
